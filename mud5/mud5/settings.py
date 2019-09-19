33e8a82f030a45314b1901af0bb9e84cb197daf4
thomas utsey <tomutsey@yahoo.com>
2019-09-19 00:27:39 -0400

    add authentication for login

--- mud5/mud5/settings.py
+++ mud5/mud5/settings.py
@@ -54,8 +54,9 @@ ROOT_URLCONF = 'mud5.urls'
 
 TEMPLATES = [
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
