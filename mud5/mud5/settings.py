88f3eb452dd50eceff1cadc293dcd9a364258a92
Javier Alavrez <javieralvarez82@live.com>
2019-09-25 22:51:23 -0700

    add apis

--- mud5/mud5/settings.py
+++ mud5/mud5/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mud5games',
     'rest_framework',
+    'mud5games',
+    
 ]
 
 MIDDLEWARE = [
--- mud5/mud5games/serializers.py
+++ mud5/mud5games/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('User_Name', 'Email', 'First_Name', 'Last_name', 'Room', 'Item')
+        fields = ('User_Name', 'Email', 'First_Name', 'Last_Name', 'Room', 'Item')
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
