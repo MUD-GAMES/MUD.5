821182262439386a865a60e081c93dc91bb9f6a2
Bao Pham <ryanvuepham@gmail.com>
2019-09-29 21:32:21 -0500

    change room

--- mud5games/management/commands/roomGen.py
+++ mud5games/management/commands/roomGen.py
@@ -2,49 +2,202 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from mud5games.models import Player, Room
 
-class Command(BaseCommand):
+# class Command(BaseCommand):
 
-    def _room_generation(self):
+    # def _room_generation(self):
 
-        Room.objects.all().delete()
+    #     Room.objects.all().delete()
 
-        r_outside = Room(Room_Name="Outside Cave Entrance",
-                       Description="North of you, the cave mount beckons")
+    #     r_outside = Room(Room_Name="Outside Cave Entrance",
+    #                    Description="North of you, the cave mount beckons")
 
-        r_foyer = Room(Room_Name="Foyer", Description="""Dim light filters in from the south. Dusty
-        passages run north and east.""")
+    #     r_foyer = Room(Room_Name="Foyer", Description="""Dim light filters in from the south. Dusty
+    #     passages run north and east.""")
 
-        r_overlook = Room(Room_Name="Grand Overlook", Description="""A steep cliff appears before you, falling
-        into the darkness. Ahead to the north, a light flickers in
-        the distance, but there is no way across the chasm.""")
+    #     r_overlook = Room(Room_Name="Grand Overlook", Description="""A steep cliff appears before you, falling
+    #     into the darkness. Ahead to the north, a light flickers in
+    #     the distance, but there is no way across the chasm.""")
 
-        r_narrow = Room(Room_Name="Narrow Passage", Description="""The narrow passage bends here from west
-        to north. The smell of gold permeates the air.""")
+    #     r_narrow = Room(Room_Name="Narrow Passage", Description="""The narrow passage bends here from west
+    #     to north. The smell of gold permeates the air.""")
 
-        r_treasure = Room(Room_Name="Treasure Chamber", Description="""You've found the long-lost treasure
-        chamber! Sadly, it has already been completely emptied by
-        earlier adventurers. The only exit is to the south.""")
+    #     r_treasure = Room(Room_Name="Treasure Chamber", Description="""You've found the long-lost treasure
+    #     chamber! Sadly, it has already been completely emptied by
+    #     earlier adventurers. The only exit is to the south.""")
+    class Room:
+    def __init__(self, id, name, description, x, y):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        if self.e_to is not None:
+            return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
+        return f"({self.x}, {self.y})"
+    def connect_rooms(self, connecting_room, direction):
+        '''
+        Connect two rooms in the given n/s/e/w direction
+        '''
+        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
+        reverse_dir = reverse_dirs[direction]
+        setattr(self, f"{direction}_to", connecting_room)
+        setattr(connecting_room, f"{reverse_dir}_to", self)
+    def get_room_in_direction(self, direction):
+        '''
+        Connect two rooms in the given n/s/e/w direction
+        '''
+        return getattr(self, f"{direction}_to")
+
+
+class World:
+    def __init__(self):
+        self.grid = None
+        self.width = 0
+        self.height = 0
+    def generate_rooms(self, size_x, size_y, num_rooms):
+        '''
+        Fill up the grid, bottom to top, in a zig-zag pattern
+        '''
+
+        # Initialize the grid
+        self.grid = [None] * size_y
+        self.width = size_x
+        self.height = size_y
+        for i in range( len(self.grid) ):
+            self.grid[i] = [None] * size_x
+
+        # Start from lower-left corner (0,0)
+        x = -1 # (this will become 0 on the first step)
+        y = 0
+        room_count = 0
+
+        # Start generating rooms to the east
+        direction = 1  # 1: east, -1: west
+
+
+        # While there are rooms to be created...
+        previous_room = None
+        while room_count < num_rooms:
+
+            # Calculate the direction of the room to be created
+            if direction > 0 and x < size_x - 1:
+                room_direction = "e"
+                x += 1
+            elif direction < 0 and x > 0:
+                room_direction = "w"
+                x -= 1
+            else:
+                # If we hit a wall, turn north and reverse direction
+                room_direction = "n"
+                y += 1
+                direction *= -1
+
+            # Create a room in the given direction
+            room = Room(room_count, "A Generic Room", "This is a generic room.", x, y)
+            # Note that in Django, you'll need to save the room after you create it
+
+            # Save the room in the World grid
+            self.grid[y][x] = room
+
+            # Connect the new room to the previous room
+            if previous_room is not None:
+                previous_room.connect_rooms(room, room_direction)
+
+            # Update iteration variables
+            previous_room = room
+            room_count += 1
 
 
 
-        r_outside.save()
-        r_foyer.save()
-        r_overlook.save()
-        r_narrow.save()
-        r_treasure.save()
+    def print_rooms(self):
+        '''
+        Print the rooms in room_grid in ascii characters.
+        '''
 
-        # Link rooms together
-        r_outside.connectRooms(r_foyer)
-        r_foyer.connectRooms(r_outside)
+        # Add top border
+        str = "# " * ((3 + self.width * 5) // 2) + "\n"
 
-        r_foyer.connectRooms(r_overlook)
-        r_overlook.connectRooms(r_foyer)
+        # The console prints top to bottom but our array is arranged
+        # bottom to top.
+        #
+        # We reverse it so it draws in the right direction.
+        reverse_grid = list(self.grid) # make a copy of the list
+        reverse_grid.reverse()
+        for row in reverse_grid:
+            # PRINT NORTH CONNECTION ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.n_to is not None:
+                    str += "  |  "
+                else:
+                    str += "     "
+            str += "#\n"
+            # PRINT ROOM ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.w_to is not None:
+                    str += "-"
+                else:
+                    str += " "
+                if room is not None:
+                    str += f"{room.id}".zfill(3)
+                else:
+                    str += "   "
+                if room is not None and room.e_to is not None:
+                    str += "-"
+                else:
+                    str += " "
+            str += "#\n"
+            # PRINT SOUTH CONNECTION ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.s_to is not None:
+                    str += "  |  "
+                else:
+                    str += "     "
+            str += "#\n"
 
-        r_foyer.connectRooms(r_narrow)
-        r_narrow.connectRooms(r_foyer)
+        # Add bottom border
+        str += "# " * ((3 + self.width * 5) // 2) + "\n"
 
-        r_narrow.connectRooms(r_treasure)
-        r_treasure.connectRooms(r_narrow)
+        # Print string
+        print(str)
+
+
+w = World()
+num_rooms = 100
+width = 8
+height = 7
+w.generate_rooms(width, height, num_rooms)
+w.print_rooms()
+
+
+print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
+
+
+        # r_outside.save()
+        # r_foyer.save()
+        # r_overlook.save()
+        # r_narrow.save()
+        # r_treasure.save()
+
+        # # Link rooms together
+        # r_outside.connectRooms(r_foyer)
+        # r_foyer.connectRooms(r_outside)
+
+        # r_foyer.connectRooms(r_overlook)
+        # r_overlook.connectRooms(r_foyer)
+
+        # r_foyer.connectRooms(r_narrow)
+        # r_narrow.connectRooms(r_foyer)
+
+        # r_narrow.connectRooms(r_treasure)
+        # r_treasure.connectRooms(r_narrow)
 
         players=Player.objects.all()
         for p in players:
@@ -52,4 +205,4 @@ class Command(BaseCommand):
           p.save()
 
     def handle(self, *args, **kwargs):
-        self._room_generation()
+        self._Room()
